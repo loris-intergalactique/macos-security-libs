68291969aa866051e2a4cef90c20c4636f357c42
bot-intergalactique <runner@fv-az95.ekfohxomoulu3czbosr3u21p3g.cx.internal.cloudapp.net>
2020-06-19 22:49:35 +0000

    build: Source update

---
+++ OSX/libsecurity_cryptkit/lib/giantPortCommon.h
@@ -0,0 +1,37 @@
+/* Copyright (c) 1998,2011,2014 Apple Inc.  All Rights Reserved.
+ *
+ * NOTICE: USE OF THE MATERIALS ACCOMPANYING THIS NOTICE IS SUBJECT
+ * TO THE TERMS OF THE SIGNED "FAST ELLIPTIC ENCRYPTION (FEE) REFERENCE
+ * SOURCE CODE EVALUATION AGREEMENT" BETWEEN APPLE, INC. AND THE
+ * ORIGINAL LICENSEE THAT OBTAINED THESE MATERIALS FROM APPLE,
+ * INC.  ANY USE OF THESE MATERIALS NOT PERMITTED BY SUCH AGREEMENT WILL
+ * EXPOSE YOU TO LIABILITY.
+ ***************************************************************************
+
+   giantPortCommon.h - common header used to specify and access
+         platform-dependent giant digit routines.
+
+ Revision History
+ ----------------
+ 1 Sep 98 at Apple
+ 	Created.
+
+*******************************/
+
+#ifndef _CRYPTKIT_GIANT_PORT_COMMON_H_
+#define _CRYPTKIT_GIANT_PORT_COMMON_H_
+
+#if  	defined(__i386__) && defined(__GNUC__)
+/* Mac OS X, Intel, Gnu compiler */
+/* This module doesn't compile yet, punt and use the 
+ * inline C functions */
+#include "giantPort_Generic.h"
+
+#else
+
+/* Others */
+#include "giantPort_Generic.h"
+
+#endif
+
+#endif	/* _CRYPTKIT_GIANT_PORT_COMMON_H_ */
